e144a46a70b3971b657b0ebf19770f75e0238e9d
Pablo Lopez <pablo.lopez.santori@gmail.com>
2019-06-11 20:22:01 +0100

    feat: Add training function in notebook
    Completed classes: DQNAgent, ReplayBuffer and Model. Also added a training function in the jupyter notebook. Additions:
    - Image preprocessing
    - act and learn methods in DQNAgent
    - train function in jupyter notebook

--- projects/capstone_project/dqn/dqnAgent.py
+++ projects/capstone_project/dqn/dqnAgent.py
@@ -1,16 +1,26 @@
-from replayBuffer import ReplayBuffer
-from model import Model
+from dqn.replayBuffer import ReplayBuffer
+from dqn.model import Model
+import numpy as np
 
 class DQNAgent:
   def __init__(self, env):
-    # Fixed target
-    self.fixed_w = None
+
+    # Hyperparameters
+    self.learning_rate = 0.01
+    self.gamma = 0.95 # discount rate
+    self.epsilon = 1 # exploration
+    self.epsilon_decay = 0.995
+    self.epsilon_min = 0.001
     
     # Model
-    self.state_size = env.observation_space.shape
+    self.state_size = (80,80,2)
     self.action_size = 6
-    self.learning_rate = 0.01
-    self.model = Model(self.state_size, self.action_size, self.learning_rate)
+    self.model = Model(self.state_size, self.action_size, self.learning_rate).model
+
+    # Model with fixed weights w-
+    self.model_f = Model(self.state_size, self.action_size, self.learning_rate).model
+    self.c_steps = 10 # how often w- gets updated
+    self.c_steps_counter = 0
 
     # Replay memory
     self.buffer_size = 100000
@@ -21,15 +31,58 @@ class DQNAgent:
     """
     Choose action using greedy policy
     """
+    if np.random.rand() <= self.epsilon:
+      # select random action
+      return np.random.choice(np.arange(self.action_size))
+
+    # select greedy action
+    act_values = self.model.predict(state)
+    return np.argmax(act_values[0])
+
+
+  def sample(self, state):
+    """
+    1. Choose action A from state S 
+    2. Take action A, observe reward R and next S
+    3. Prepare next state S'
+    4. Store experience tuple (S,A,R,S') in replay memory
+    5. S <- S'
+    """
     pass
 
-  def learn(self):
+  def update_model_f(self):
+    """
+    Updates fixed weights of the 'model_f' every C steps.
+    """
+    if self.c_steps_counter == self.c_steps:
+      self.model_f.set_weights(self.model.get_weights()) 
+      self.c_steps_counter = 0
+    else:
+      self.c_steps_counter += 1
+
+  def learn(self, batch_size=32):
     """
     1. Obtain a batch of random samples
-    2. Set target y
+    2. Set target y = r + gamma*max q(s,a,w-)
     3. Update weights (forward pass -> Gradient descent)
-    4. Update fixed w after C steps
+    4. Update fixed w after C steps w- <- w
     """
-    pass
+    minibatch = self.memory.sample(batch_size)
+    
+    for state, action, reward, next_state, done in minibatch:
+      target = reward
+      if not done:
+        target = reward + self.gamma * np.amax(self.model_f.predict(next_state)[0])
+
+      # Convert target to have the same shape as output
+      target_f = self.model.predict(state)
+      target_f[0][action] = target
+
+      self.model.fit(state, target_f, epochs=1, verbose=0)
+      self.update_model_f()
+
+    if self.epsilon > self.epsilon_min:
+      self.epsilon *= self.epsilon_decay
+
 
   
--- projects/capstone_project/dqn/model.py
+++ projects/capstone_project/dqn/model.py
@@ -1,6 +1,9 @@
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation, BatchNormalization, GlobalAveragePooling2D
 from keras.optimizers import Adam
+from PIL import Image
+import numpy as np
+
 
 class Model:
   def __init__(self, state_size, action_size, learning_rate):
@@ -13,15 +16,15 @@ class Model:
   def build_model(self):
     model = Sequential()
     model.add(Conv2D(filters=32, kernel_size=(8,8), padding='same', activation='relu', 
-                      strides=4, input_shape=self.state_size))
+                      strides=2, input_shape=self.state_size))
     #model.add(MaxPooling2D(pool_size=2))
 
     model.add(Conv2D(filters=64, kernel_size=(4,4), padding='same', activation='relu',
-                      strides=4))
+                      strides=2))
     #model.add(MaxPooling2D(pool_size=2))
 
     model.add(Conv2D(filters=64, kernel_size=(3,3), padding='same', activation='relu',
-                      strides=4,))
+                      strides=2))
     #model.add(MaxPooling2D(pool_size=2))
 
     model.add(Flatten())
@@ -33,3 +36,16 @@ class Model:
     model.compile(optimizer=Adam(lr=self.learning_rate), loss='mse')
 
     return model
+
+  @staticmethod
+  def img_to_tensor(img):
+    # convert 3D tensor to 4D tensor with shape (1, n, n, M) and return 4D tensor
+    return np.expand_dims(img, axis=0)
+
+  @staticmethod
+  def img_preprocessing(img):
+    img = Image.fromarray(img)
+    img = img.resize((80,80)).convert('LA')
+
+    return Model.img_to_tensor(img)
+
