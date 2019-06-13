54c6fef4d37961ea2a4bd8e35a9c8b207b8f2e0d
Pablo Lopez <pablo.lopez.santori@gmail.com>
2019-06-13 12:26:25 +0100

    feat: Add utils class to stack K images in a state
    Included the class from OPEN AI baselines to stack k images in a single state

--- projects/capstone_project/dqn/model.py
+++ projects/capstone_project/dqn/model.py
@@ -42,13 +42,13 @@ class Model:
   def img_preprocessing(img):
     x_t = Image.fromarray(img)
     area = (0, 20, 160 , 196)
-    x_t = np.asarray(x_t.crop(area))
-    x_t = skimage.color.rgb2gray(x_t)
-    x_t = skimage.transform.resize(x_t,(80,80))
+    x_t = np.asarray(x_t.crop(area)) # crop image
+    x_t = skimage.color.rgb2gray(x_t) # srayscale
+    x_t = skimage.transform.resize(x_t,(80,80)) # scale
     x_t = skimage.exposure.rescale_intensity(x_t, out_range=(0, 255))
     x_t = x_t/255.
 
-    s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
+    s_t = np.stack((x_t, x_t, x_t, x_t), axis=2) # stack 4 images
     s_t = s_t.reshape(1, s_t.shape[0], s_t.shape[1], s_t.shape[2])
 
     return s_t
